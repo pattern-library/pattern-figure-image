07e56e328ac788ea81523a9eb21ce43858dfd70f
Scott Nath <snath@us.ibm.com>
2017-05-11 16:44:08 -0400

    :new: core module/pattern setup

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Figure-Image pattern
+ * Documents a system-agnostic HTML pattern for a figure element containing an image
+ * @module figureImage
+ */
+
+module.exports = {
+  name: 'figure-image',
+  description: 'A system-agnostic HTML pattern for a figure element containing an image',
+  content: {
+    figure: {
+      class: 'figure--image',
+    },
+    img: {
+      alt: 'Foo Image',
+      src: 'https://unsplash.it/200/300',
+    },
+    figcaption: {
+      content: 'Foo image caption',
+    },
+  },
+  versions: {
+    default: './src/figure-image.html',
+  },
+};
